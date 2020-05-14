c4791eb4b88e5ba4fc174fba2932d34f416d99ae
Carlos Araujo <Carlos.Araujo@verndale.com>
2020-05-14 12:47:23 -0500

    completing B1 exercise

---
+++ AlloyTraining/Controllers/StartPageController.cs
@@ -0,0 +1,14 @@
+﻿using AlloyTraining.Models.Pages;
+using EPiServer.Web.Mvc;
+using System.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ AlloyTraining/Models/Pages/StartPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "c8846ea4-11f1-4bfd-b72b-ab7dabd39f92", 
+        GroupName = SiteGroupNames.Specialized, Order = 10,
+        Description = "The home page for a website with an area for blocks and partial pages.")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Heading",
+            Description = "If the Heading is not set, the page falls back to showing the Name.",
+            GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Main body",
+            Description = "The main body uses the XHTML-editor you can insert for example text, images, and tables.",
+            GroupName = SystemTabNames.Content, Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [Display(Name = "Main content area",
+            Description = "The main content area contains an ordered collection to content references, for example blocks, media assets, and pages.",
+            GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual ContentArea MainContentArea { get; set; }
+    }
+}
