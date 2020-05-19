0f5ff0ad852129e1e5140db4c0f37e45181f8322
Carlos Araujo <Carlos.Araujo@verndale.com>
2020-05-19 14:46:35 -0500

    changes made C2-C5

---
+++ AlloyTraining/Controllers/StartPageMobileController.cs
@@ -0,0 +1,22 @@
+﻿using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
+using System.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    [TemplateDescriptor(Tags = new[] { RenderingTags.Mobile }, AvailableWithoutTag = false)]
+    public class StartPageMobileController : PageControllerBase<StartPage>
+    {
+        public StartPageMobileController(IContentLoader loader) : base(loader)
+        {
+        }
+
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
